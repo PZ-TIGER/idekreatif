e1bffea700cadc7219fff65b249c6770051daa77
PZ-TIGER <yurifarel99@gmail.com>
2025-01-14 15:26:21 +0700

    menambahkan kode pada proses kategori php

---
+++ proses_kategori.php
@@ -0,0 +1,34 @@
+<?php
+
+
+include("config.php");
+
+
+session_start();
+
+
+if (isset($_POST['simpan'])) {
+
+    $category_name = $_POST['category_name'];
+
+
+    $query = "INSERT INTO categories (category_name) VALUES('$category_name')";
+    $exec = mysqli_query($conn, $query);
+
+
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'kategori berhasil di tambahkan!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal menambahkan kategori; ' .myqsli_error($conn)
+        ];
+    }
+
+    //redirect kembali ke halaman kategori
+    header('Location: kategori.php');
+    exit();
+}
